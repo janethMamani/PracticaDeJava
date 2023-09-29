3b81a29b02790e110e7f607c6aa90047be63bc36
Janeth Magdalena <janethmamani0403@gmail.com>
2023-09-29 18:16:25 -0300

    vectores

---
+++ src/vectores.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class vectores {
+    public static void main(String[] args) {
+        //Declaracion
+        int vector [] = new int [4];
+        //Asignacion por teclado
+        Scanner teclado = new Scanner(System.in);
+        for (int i=0; i<vector.length; i++){
+            System.out.println("Ingrese el valor para el indice "+i);
+            vector [i] =teclado.nextInt();
+        }
+        //Asignacion (manual)
+        //vector[0]=2;
+        //vector[1]=35;
+        //vector[2]=48;
+        //vector[3]=157;
+
+        //Recorrido
+        for (int i=0; i<vector.length; i++){
+            System.out.println("Estoy en el indice: " + i);
+            System.out.println("Tengo guardado un " + vector[i]);
+            System.out.println("----------------");
+        }
+    }
+}
